ad195f6b4e82bfc2f14296fe054305aac616a850
muscaiu <muscaiu@yahoo.com>
2016-11-08 12:43:17 +0200

    send message from front-end to  back-end and save in Mongo

--- back-end/server.js
+++ back-end/server.js
@@ -3,6 +3,8 @@ var app = express()
 var bodyParser = require('body-parser')
 var mongo = require('mongodb').MongoClient
 
+var database
+
 var server = app.listen(5000, function(){
     console.log('express listening on port', server.address().port)
 })
@@ -11,6 +13,7 @@ app.use(bodyParser.json())
 
 app.post('/api/message', function(req, res){
     console.log(req.body)
+    database.collection('messages').insertOne(req.body)
     res.status(200)
 })
 
@@ -25,13 +28,11 @@ app.use(function(req, res, next){
 mongo.connect('mongodb://localhost:27017/test', function(err, db){
     if(!err){
         console.log('connected to mongo')
-        db.collection('messages').insertOne({'msg':'test'})
+        database = db
     }
     else(console.log(err))
 })
 
 //how to test this:
-//before node server.js -> run mongod --dbpath D:\MEAN\MyLyndaMEAN\db
-//mongo
-//show collections
-//db.messages.find()
+//rerun node server and do a post from the form on the page
+//TESTED OK!!!
