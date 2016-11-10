3f2d53f69e859e0a17a6b9822a1f2821feb3f658
muscaiu <muscaiu@yahoo.com>
2016-11-10 12:09:31 +0200

    including dist/

--- front-end/src/app/main/main.controller.js
+++ front-end/src/app/main/main.controller.js
@@ -9,12 +9,12 @@ export class MainController {
   getMessages(){
     var vm = this
 
-    this.$http.get('http://localhost:5000/api/message').then(function(result){
+    this.$http.get('http://54.229.174.169:8080/api/message').then(function(result){
       vm.messages = result.data
     })
   }
 
   postMessage(){
-    this.$http.post('http://localhost:5000/api/message', {msg:this.message})
+    this.$http.post('http://54.229.174.169:8080/api/message', {msg:this.message})
   }
 }
